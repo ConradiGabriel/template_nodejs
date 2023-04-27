96e7084ab94375f933ed65c953a69a32db974c68
conradigabriel <conradigabriel@icloud.com>
2023-04-26 21:47:33 -0300

    commit

---
+++ app-backend-nodejs/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World! Gabriel')
+})
+app.use(express.json());
+app.post('/filmes',(req,res) => {
+const {titulo, genero} = req.body;
+res.send(`Filme: ${titulo} - Genero: ${genero}, recebido`);
+});
+app.post('/livros',(req,res) => {
+const {autor, nome, editora} = req.body;
+res.send(`Autor: ${autor} - Nome: ${nome} - Editora ${editora}, recebido`);
+});
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
